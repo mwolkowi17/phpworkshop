86b6e1cc82beaa2257b2800b25ab5a3bdcb18947
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-01-21 09:13:21 +0100

    new lisr

--- dbtest.php
+++ dbtest.php
@@ -7,23 +7,35 @@ $dbpassword = "";
 $db_conn = new PDO("mysql:host=".$dbhost.";dbname=".$dbname, $dbuser, $dbpassword);
 
 $stmt = $db_conn->query('SELECT name, year FROM movies'); 
-
-while($row = $stmt->fetch()) {  echo $row['name'] . ': rok ' . $row['year'] . "\n"; } 
+print_r($stmt);
 ?>
 <p></p>
 <?php
-$sql = "SELECT name, description, year  FROM movies WHERE id = :movie_id";
+while($row = $stmt->fetch()) {?><p><?php  echo $row['name'] . ': rok ' . $row['year'] . "\n";?></p><?php } 
+?>
+<p></p>
+<?php
+
+?>
+<p></p>
+<?php
+//$sql = "SELECT name, description, year  FROM movies WHERE id = :movie_id";
+$sql = "SELECT name, description, year  FROM movies";
 $stmt = $db_conn->prepare($sql);
 $stmt->execute(array(
-   "movie_id" => 1)
+   //"movie_id" => 1
+   )
 );
 $movie = $stmt->fetchAll(PDO::FETCH_ASSOC);
 print_r($movie);
+foreach($movie as $v){?><p><?php
+   echo $v['name'] . ' rok:' . $v['year'];?></p><?php
+}
 
 ?>
 <p></p>
 <?php
-$sql = 'INSERT INTO movies (name, description, year, created)
+/*$sql = 'INSERT INTO movies (name, description, year, created)
  VALUES (:name, :description, :year, NOW())';
 $stmt = $db_conn->prepare($sql);
 $stmt->execute(array(
@@ -34,5 +46,5 @@ $stmt->execute(array(
 
  $sql = 'UPDATE movies SET category_id = :category_id  WHERE id = :movie_id'; 
  $stmt = $db_conn->prepare($sql); $stmt->execute(array(':category_id' => 2, ':movie_id' => 5)); 
- echo 'Zmieniono '.$stmt->rowCount().' rekordów';
+ echo 'Zmieniono '.$stmt->rowCount().' rekordów';*/
 
